322a76752dd499167346f4dba9c5a6713ff88168
Ralph Lee <ralph.lee@live.ca>
2021-06-29 15:55:22 +0900

    Create a SideMenu component

---
+++ sis-administrator-interface/src/components/SideMenu.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <b-button v-b-toggle.sidebar-no-header>Open Sidebar</b-button>
+    <b-sidebar
+      id="sidebar-no-header"
+      aria-labelledby="sidebar-no-header-title"
+      no-header
+      shadow
+    >
+      <template #default="{ hide }">
+        <div class="p-3">
+          <h4 id="sidebar-no-header-title">Swift Learning</h4>
+          <img src="../assets/Swift Learning Logo.png" alt="" />
+          <nav class="mb-3">
+            <b-nav vertical>
+              <b-nav-item active @click="hide">Dashboard</b-nav-item>
+              <b-nav-item href="#link-1" @click="hide"
+                >Course Reports</b-nav-item
+              >
+              <b-nav-item href="#link-2" @click="hide"
+                >Registration Reports</b-nav-item
+              >
+            </b-nav>
+          </nav>
+          <b-button variant="primary" block @click="hide"
+            >Close Sidebar</b-button
+          >
+        </div>
+      </template>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideMenu",
+};
+</script>
+
+<style scoped></style>
