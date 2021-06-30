4b49e1620cb8842a9929ad216879c13cbe415519
Ralph Lee <ralph.lee@live.ca>
2021-06-30 11:59:08 +0900

    Insert Navbar and SideMenu components

--- sis-administrator-interface/src/App.vue
+++ sis-administrator-interface/src/App.vue
@@ -1,44 +1,20 @@
 <template>
   <div id="app">
-    <div>
-      <b-button v-b-toggle.sidebar-no-header>Open Sidebar</b-button>
-      <b-sidebar
-        id="sidebar-no-header"
-        aria-labelledby="sidebar-no-header-title"
-        no-header
-        shadow
-      >
-        <template #default="{ hide }">
-          <div class="p-3">
-            <h4 id="sidebar-no-header-title">Swift Learning</h4>
-            <img src="./assets/Swift Learning Logo.png" alt="" />
-            <nav class="mb-3">
-              <b-nav vertical>
-                <b-nav-item active @click="hide">Dashboard</b-nav-item>
-                <b-nav-item href="#link-1" @click="hide"
-                  >Course Reports</b-nav-item
-                >
-                <b-nav-item href="#link-2" @click="hide"
-                  >Registration Reports</b-nav-item
-                >
-              </b-nav>
-            </nav>
-            <b-button variant="primary" block @click="hide"
-              >Close Sidebar</b-button
-            >
-          </div>
-        </template>
-      </b-sidebar>
-    </div>
+    <Navbar>Course Reports</Navbar>
+    <SideMenu></SideMenu>
   </div>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar.vue";
+import SideMenu from "./components/SideMenu.vue";
 
 export default {
   name: "App",
-  components: {},
+  components: {
+    Navbar,
+    SideMenu,
+  },
 };
 </script>
 
