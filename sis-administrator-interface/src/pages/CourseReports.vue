88ae9b196c920df74f88b4bc1725285b819b5899
Ralph Lee <ralph.lee@live.ca>
2021-07-01 14:33:31 +0900

    Create CourseReports component

---
+++ sis-administrator-interface/src/pages/CourseReports.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Navbar>Course Reports</Navbar>
+    <SideMenu></SideMenu>
+    <h2>This is Course Reports page</h2>
+  </div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+import SideMenu from "../components/SideMenu.vue";
+
+export default {
+  name: "CourseReports",
+  components: {
+    Navbar,
+    SideMenu,
+  },
+};
+</script>
+
+<style></style>
