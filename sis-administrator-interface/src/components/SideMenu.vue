15dbb175e81146ad9c0371b015dc792b315ad373
Ralph Lee <ralph.lee@live.ca>
2021-07-01 14:32:38 +0900

    Add dark variant

--- sis-administrator-interface/src/components/SideMenu.vue
+++ sis-administrator-interface/src/components/SideMenu.vue
@@ -4,27 +4,60 @@
     <b-sidebar
       id="sidebar-no-header"
       aria-labelledby="sidebar-no-header-title"
+      :backdrop-variant="light"
+      backdrop
       no-header
       shadow
     >
       <template #default="{ hide }">
         <div class="p-3">
-          <h4 id="sidebar-no-header-title">Swift Learning</h4>
-          <img src="../assets/Swift Learning Logo.png" alt="" />
+          <!-- <h4 id="sidebar-no-header-title">Swift Learning</h4> -->
+          <img
+            class="side-menu-logo"
+            src="../assets/Swift Learning Logo.png"
+            alt=""
+            height="100px"
+          />
           <nav class="mb-3">
             <b-nav vertical>
-              <b-nav-item active @click="hide">Dashboard</b-nav-item>
-              <b-nav-item href="#link-1" @click="hide"
-                >Course Reports</b-nav-item
-              >
-              <b-nav-item href="#link-2" @click="hide"
-                >Registration Reports</b-nav-item
-              >
+              <h5>Dashboard</h5>
+              <b-nav-item active @click="hide">Home</b-nav-item>
+              <hr />
+
+              <h5>Reports</h5>
+              <b-nav-item href="../pages/CourseReports.vue" @click="hide">
+                Course Reports
+              </b-nav-item>
+              <b-nav-item href="#link-2" @click="hide">
+                Registration Reports
+              </b-nav-item>
+              <b-nav-item href="#link-2" @click="hide">
+                Contact Reports
+              </b-nav-item>
+              <hr />
+
+              <h5>Registration</h5>
+              <b-nav-item href="#link-1" @click="hide">
+                Global Admin
+              </b-nav-item>
+              <b-nav-item href="#link-2" @click="hide">
+                Primary Admin
+              </b-nav-item>
+              <b-nav-item href="#link-2" @click="hide">
+                Teachers
+              </b-nav-item>
+              <b-nav-item href="#link-2" @click="hide">
+                Divisions
+              </b-nav-item>
+              <hr />
+
+              <h5>Course Access</h5>
+              <b-nav-item href="#link-1" @click="hide">
+                Assign Access
+              </b-nav-item>
             </b-nav>
           </nav>
-          <b-button variant="primary" block @click="hide"
-            >Close Sidebar</b-button
-          >
+          <b-button variant="primary" block @click="hide">Close</b-button>
         </div>
       </template>
     </b-sidebar>
@@ -34,7 +67,15 @@
 <script>
 export default {
   name: "SideMenu",
+  data() {
+    return {
+      variant: "dark",
+    };
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.side-menu-logo {
+}
+</style>
