2adb42c91826d30e7e8dc41d7958194cedf25315
Ralph Lee <ralph.lee@live.ca>
2021-07-05 18:54:11 +0900

    Add a search bar

--- sis-administrator-interface/src/App.vue
+++ sis-administrator-interface/src/App.vue
@@ -2,7 +2,29 @@
   <div id="app">
     <Navbar>Course Reports</Navbar>
     <SideMenu></SideMenu>
+    <b-col lg="6" class="my-1">
+      <b-form-group
+        label="Filter"
+        label-for="filter-input"
+        label-cols-sm="3"
+        label-align-sm="right"
+        label-size="sm"
+        class="mb-0"
+      >
+        <b-input-group size="sm">
+          <b-form-input
+            id="filter-input"
+            v-model="filter"
+            type="search"
+            placeholder="Type to Search"
+          ></b-form-input>
 
+          <b-input-group-append>
+            <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </b-form-group>
+    </b-col>
     <div class="main-content">
       <div class="filter">
         <h4>Filter By</h4>
