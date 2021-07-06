0a3a1928eaf5f595c7d66501652b27c3fee175fb
Ralph Lee <ralph.lee@live.ca>
2021-07-06 22:48:52 +0900

    Add search and pagination

--- sis-administrator-interface/src/App.vue
+++ sis-administrator-interface/src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <div id="app">
+  <div>
     <Navbar>Course Reports</Navbar>
     <SideMenu></SideMenu>
-    <b-col lg="6" class="my-1">
+
+    <!-- --------Search bar-------- -->
+    <!-- <b-col lg="6" class="my-1">
       <b-form-group
-        label="Filter"
+        label="Search"
         label-for="filter-input"
         label-cols-sm="3"
         label-align-sm="right"
@@ -24,7 +26,8 @@
           </b-input-group-append>
         </b-input-group>
       </b-form-group>
-    </b-col>
+    </b-col> -->
+
     <div class="main-content">
       <div class="filter">
         <h4>Filter By</h4>
@@ -96,24 +99,121 @@
         </b-form-group>
       </div>
 
-      <b-table
+      <b-container fluid id="app">
+        <!-- User Interface controls -->
+        <b-row>
+          <b-col lg="6" class="my-1">
+            <b-form-group
+              label="Search"
+              label-for="filter-input"
+              label-cols-sm="1"
+              label-align-sm="right"
+              label-size="sm"
+              class="mb-0"
+            >
+              <b-input-group size="sm">
+                <b-form-input
+                  id="filter-input"
+                  v-model="filter"
+                  type="search"
+                  placeholder="Type to Search"
+                ></b-form-input>
+
+                <b-input-group-append>
+                  <b-button :disabled="!filter" @click="filter = ''"
+                    >Clear</b-button
+                  >
+                </b-input-group-append>
+              </b-input-group>
+            </b-form-group>
+          </b-col>
+
+          <b-col sm="7" md="6" class="my-1">
+            <b-pagination
+              v-model="currentPage"
+              :total-rows="totalRows"
+              :per-page="perPage"
+              align="right"
+              size="sm"
+              class="my-0"
+            ></b-pagination>
+          </b-col>
+        </b-row>
+
+        <!-- Main table element -->
+        <b-table
+          :items="items"
+          :fields="fields"
+          :current-page="currentPage"
+          :per-page="perPage"
+          :filter="filter"
+          :filter-included-fields="filterOn"
+          :sort-by.sync="sortBy"
+          :sort-desc.sync="sortDesc"
+          :sort-direction="sortDirection"
+          stacked="sm"
+          show-empty
+          head-variant="light"
+          hover
+          @filtered="onFiltered"
+        >
+          <template #cell(name)="row">
+            {{ row.value.first }} {{ row.value.last }}
+          </template>
+
+          <!-- Action buttons for modal and details -->
+          <!-- <template #cell(actions)="row">
+          <b-button
+            size="sm"
+            @click="info(row.item, row.index, $event.target)"
+            class="mr-1"
+          >
+            Info modal
+          </b-button>
+          <b-button size="sm" @click="row.toggleDetails">
+            {{ row.detailsShowing ? "Hide" : "Show" }} Details
+          </b-button>
+        </template> -->
+
+          <template #row-details="row">
+            <b-card>
+              <ul>
+                <li v-for="(value, key) in row.item" :key="key">
+                  {{ key }}: {{ value }}
+                </li>
+              </ul>
+            </b-card>
+          </template>
+        </b-table>
+
+        <!-- Info modal -->
+        <b-modal
+          :id="infoModal.id"
+          :title="infoModal.title"
+          ok-only
+          @hide="resetInfoModal"
+        >
+          <pre>{{ infoModal.content }}</pre>
+        </b-modal>
+      </b-container>
+
+      <!-- <b-table
         id="table-transition-example"
+        class="table"
         outlined
         head-variant="light"
         hover
         :items="items"
         :fields="fields"
+        :filter="filter"
         primary-key="index"
         :tbody-transition-props="transProps"
       >
-        <template #cell(index)="data">
-          {{ data.index + 1 }}
-        </template>
         <template #cell(name)="data">
           <b class="text-info">{{ data.value.last }}</b
           >, <b>{{ data.value.first }}</b>
         </template>
-      </b-table>
+      </b-table> -->
     </div>
   </div>
 </template>
@@ -130,23 +230,31 @@ export default {
   },
   data() {
     return {
-      transProps: {
-        // Transition name
-        name: "flip-list",
-      },
-      fields: [
-        // { key: "Course", sortable: true },
-        "index",
-        // "name",
-        { key: "name", label: "Full Name", sortable: true },
-
-        "age",
-        "school",
-        "course",
-        "group",
-        "division",
-      ],
       items: [
+        {
+          age: 40,
+          name: { first: "Jackerson", last: "Macdonald" },
+        },
+        { age: 21, name: { first: "Larry", last: "Shall" } },
+        {
+          age: 9,
+          name: { first: "Mini", last: "Navarro" },
+        },
+        { age: 89, name: { first: "Geneva", last: "Wilson" } },
+        { age: 38, name: { first: "Jamie", last: "Carson" } },
+        { age: 27, name: { first: "Essie", last: "Dunlap" } },
+        { age: 40, name: { first: "Thor", last: "Macdonald" } },
+        {
+          age: 87,
+          name: { first: "Larsen", last: "Shaw" },
+        },
+        { age: 26, name: { first: "Mitzi", last: "Navarro" } },
+        {
+          age: 22,
+          name: { first: "Genevieve", last: "Wilson" },
+        },
+        { age: 38, name: { first: "John", last: "Carney" } },
+        { age: 29, name: { first: "Rick", last: "Dunlap" } },
         {
           name: { first: "Tommy", last: "Miller" },
           age: 30,
@@ -160,7 +268,7 @@ export default {
           course: "ASBESTOS AWARENESS- BC",
         },
         {
-          name: { first: "Jack", last: "Macdonald" },
+          name: { first: "Julia", last: "Vanderwal" },
           age: 20,
           school: "Airdrie Koinonia Christian School",
           course: "ALBERTA DRIVER HOURS OF SERVICE LIMITS",
@@ -184,9 +292,139 @@ export default {
           course: "EMERGENCY RESPONSE PLANNING (ERP)",
         },
       ],
+      fields: [
+        {
+          key: "name",
+          label: "Full name",
+          sortable: true,
+          sortDirection: "desc",
+        },
+        {
+          key: "age",
+          sortable: true,
+        },
+        {
+          key: "school",
+          sortable: true,
+          sortByFormatted: true,
+          filterByFormatted: true,
+        },
+        { key: "course", sortable: true },
+      ],
+      totalRows: 1,
+      currentPage: 1,
+      perPage: 10,
+      sortBy: "",
+      sortDesc: false,
+      sortDirection: "asc",
+      filter: null,
+      filterOn: [],
+      infoModal: {
+        id: "info-modal",
+        title: "",
+        content: "",
+      },
     };
   },
+  computed: {
+    sortOptions() {
+      // Create an options list from our fields
+      return this.fields
+        .filter((f) => f.sortable)
+        .map((f) => {
+          return { text: f.label, value: f.key };
+        });
+    },
+  },
+  mounted() {
+    // Set the initial number of items
+    this.totalRows = this.items.length;
+  },
+  methods: {
+    info(item, index, button) {
+      this.infoModal.title = `Row index: ${index}`;
+      this.infoModal.content = JSON.stringify(item, null, 2);
+      this.$root.$emit("bv::show::modal", this.infoModal.id, button);
+    },
+    resetInfoModal() {
+      this.infoModal.title = "";
+      this.infoModal.content = "";
+    },
+    onFiltered(filteredItems) {
+      // Trigger pagination to update the number of buttons/pages due to filtering
+      this.totalRows = filteredItems.length;
+      this.currentPage = 1;
+    },
+  },
 };
+
+// ---------------------------------------------
+// export default {
+//   name: "App",
+//   components: {
+//     Navbar,
+//     SideMenu,
+//   },
+//   data() {
+//     return {
+//       transProps: {
+//         // Transition name
+//         name: "flip-list",
+//       },
+//       fields: [
+//         // { key: "Course", sortable: true },
+//         "index",
+//         // "name",
+//         { key: "name", label: "Full Name", sortable: true },
+
+//         "age",
+//         "school",
+//         "course",
+//         "group",
+//         "division",
+//       ],
+//       items: [
+//         {
+//           name: { first: "Tommy", last: "Miller" },
+//           age: 30,
+//           school: "Alberta High School of Fine Arts",
+//           course: "BEAR SAFETY AWARENESS - SK",
+//         },
+//         {
+//           name: { first: "Lauren", last: "Braun" },
+//           age: 25,
+//           school: "Renert School",
+//           course: "ASBESTOS AWARENESS- BC",
+//         },
+//         {
+//           name: { first: "Jack", last: "Macdonald" },
+//           age: 20,
+//           school: "Airdrie Koinonia Christian School",
+//           course: "ALBERTA DRIVER HOURS OF SERVICE LIMITS",
+//         },
+//         {
+//           name: { first: "Larsen", last: "Shaw" },
+//           age: 21,
+//           school: "Old Scona School",
+//           course: "BULLYING PREVENTION",
+//         },
+//         {
+//           name: { first: "Geneva", last: "Wilson" },
+//           age: 18,
+//           school: "Webber Academy",
+//           course: "COLD STRESS AWARENESS",
+//         },
+//         {
+//           name: { first: "Jami", last: "Carney" },
+//           age: 24,
+//           school: "Fraser Valley High",
+//           course: "EMERGENCY RESPONSE PLANNING (ERP)",
+//         },
+//       ],
+//       filter: null,
+//     };
+//   },
+// };
 </script>
 
 <style>
@@ -206,6 +444,12 @@ export default {
 .filter {
   border: 1px solid black;
   width: 20%;
+  margin: 10px;
+  padding: 10px;
+}
+
+.table {
+  margin: 10px;
 }
 
 table#table-transition-example .flip-list-move {
