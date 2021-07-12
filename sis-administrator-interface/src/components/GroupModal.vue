8fb7c251b574835fd8902e4cc36da1f4eb3c663c
Ralph Lee <ralph.lee@live.ca>
2021-07-12 14:45:55 +0900

    Create a modal for filtering by groups

--- sis-administrator-interface/src/components/GroupModal.vue
+++ sis-administrator-interface/src/components/GroupModal.vue
@@ -2,24 +2,27 @@
   <div>
     <!-- <b-button v-b-modal.modal-scrollable><slot></slot></b-button> -->
     <slot />
-    <b-modal id="modal-scrollable" scrollable title="Select Courses">
+    <b-modal id="group-modal" scrollable title="Select Groups">
       <b-form-checkbox id="checkbox-1" name="checkbox-1" value="accepted">
-        BEAR SAFETY AWARENESS - SK
+        Federal
       </b-form-checkbox>
       <b-form-checkbox id="checkbox-2" name="checkbox-2" value="accepted">
-        ASBESTOS AWARENESS- BC
+        First Nations
       </b-form-checkbox>
       <b-form-checkbox id="checkbox-3" name="checkbox-3" value="accepted">
-        ALBERTA DRIVER HOURS OF SERVICE LIMITS
+        Francophone
       </b-form-checkbox>
       <b-form-checkbox id="checkbox-4" name="checkbox-4" value="accepted">
-        BULLYING PREVENTION
+        Private
       </b-form-checkbox>
       <b-form-checkbox id="checkbox-5" name="checkbox-5" value="accepted">
-        COLD STRESS AWARENESS
+        Provincial
       </b-form-checkbox>
       <b-form-checkbox id="checkbox-6" name="checkbox-6" value="accepted">
-        EMERGENCY RESPONSE PLANNING (ERP)
+        Public
+      </b-form-checkbox>
+      <b-form-checkbox id="checkbox-6" name="checkbox-6" value="accepted">
+        Separate
       </b-form-checkbox>
     </b-modal>
   </div>
