afd432454f9132ab46cd319ad2ecd7a7fb790136
Ralph Lee <ralph.lee@live.ca>
2021-07-25 23:41:15 +0900

    Create SimpleData component

---
+++ sis-administrator-interface/src/components/SimpleData.vue
@@ -0,0 +1,22 @@
+<template>
+  <h4 class="simple-data">Total {{ specificName }}: {{ totalNumber }}</h4>
+</template>
+
+<script>
+export default {
+  name: "SimpleData",
+  props: ["specificName", "totalNumber"],
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.simple-data {
+  /* border: 1px solid black; */
+  width: 100%;
+  margin: 10px;
+  padding: 10px;
+}
+</style>
