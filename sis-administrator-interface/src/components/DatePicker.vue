4e0e4afa248f838288c8b1ad14f65b72e986f32b
Ralph Lee <ralph.lee@live.ca>
2021-07-26 12:41:57 +0900

    Create DatePicker component

---
+++ sis-administrator-interface/src/components/DatePicker.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <label for="datepicker"><slot></slot> date</label>
+    <b-form-datepicker
+      v-model="selectedDate"
+      class="mb-2 date-box"
+      :max="maxDate"
+      @input="updatedDate"
+    ></b-form-datepicker>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DatePicker",
+  props: ["selected", "maxDate"],
+  data() {
+    return {
+      selectedDate: this.selected,
+    };
+  },
+  methods: {
+    updatedDate() {
+      this.$emit("updatedDate", this.selectedDate);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.date-box {
+  font-size: 0.8vw;
+}
+</style>
